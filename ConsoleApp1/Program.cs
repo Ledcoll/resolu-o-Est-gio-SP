295953fabacc149586e8875d55d94a9769b6bcac
Ledcoll <marcoantredi@gmail.com>
2024-09-23 13:50:21 -0300

    resolucao 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿int indice = 13;
+int soma = 0;
+int k = 0;
+
+for (indice = 13; indice > k; k = +1 )
+{
+ soma = soma + k;
+
+ if (soma == 13)
+ {
+    break;
+ }
+}
+Console.WriteLine("O valor de soma é: " + soma);
+Console.ReadKey();
